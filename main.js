7a6c4070379811c3b357eaee58a99d7a8c9d5a0a
analu001 <152643733+analu001@users.noreply.github.com>
2024-10-02 07:34:07 -0300

    oi

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
-function soma (num1,num2){
-    return num1+num2;
+// function soma (num1,num2){
+//     return num1+num2;
 
-}
- console.log(soma(5,2));
+// }
+//  console.log(soma(5,2));
 
- function areaQuadrado(lado){
+//  function areaQuadrado(lado){
 
-return lado*lado;
- }
- console.log(areaQuadrado(6));
+// return lado*lado;
+//  }
+//  console.log(areaQuadrado(6));
 
- let num5=parseInt(prompt('digite um número'));
- let num2=parseInt(prompt('digite um número'));
- let num1=parseInt(prompt('digite um número'));
+//  let num5=parseInt(prompt('digite um número'));
+//  let num2=parseInt(prompt('digite um número'));
+//  let num1=parseInt(prompt('digite um número'));
 
- let calculoMedia= function(num5,num2,num1){
-    return (num5+num2+num1)/3;
+//  let calculoMedia= function(num5,num2,num1){
+//     return (num5+num2+num1)/3;
 
- }
+//  }
  
- document.write(calculoMedia(num5,num2,num1));
+//  document.write(calculoMedia(num5,num2,num1));
+
+
+
+
+let estados=[`pr`,`rs`,`mg`];
