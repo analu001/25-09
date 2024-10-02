7cbd9af19ffd6a735d16a302e7173d513af2f4cd
analu001 <152643733+analu001@users.noreply.github.com>
2024-10-02 08:28:16 -0300

    k

--- main.js
+++ main.js
@@ -36,7 +36,7 @@
 //         console.log(estados[i]);
 //     }
 
-let idade=parseInt(prompt( 'qual é a sua idade?'));
+let idade=parseInt(prompt('qual é a sua idade?'));
 if(idade>17){ 
     console.log('pode dirigir');
 }
